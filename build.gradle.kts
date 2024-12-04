063b605f3d2bf1d3816df21f05d2feede1cc8b77
Michael Kaulig <michael.kaulig@meisterplan.com>
2024-12-04 09:10:17 +0100

    Fix mockito test to have strict mocking
    This commit shows, that it is possible to have a high strictness when
    using mockito as a mocking library
    
    - the mockito session must be active during the mock creation. This
      can be achieved by using @ExtendWith(MockitoExtension::class) in
      combination with @Mock to initialize mocks. Alternatively mocks
      can be initialized before each test. You can not initialize mocks
      during the class instantiation, because at this point the mockito
      session is not yet initialized.
    - What mockito calls strict mocking does only detect cases where method
      calls were stubbed but not called during the test. This prevents dead
      stubbing code.
    - We also want to have the guarantee to detect if methods were called on
      the mock, that were not explicitly stubbed. Mockito returns
      automatically default values in this case even with stubbing. To
      change this, we need to provide an alternative default answer, that
      throws an exception, which is done with the mockStrictly() helper.
      Unfortunately mockito does not allow to use custom default answers
      in combination with the @Mock initialization. That is why we have
      to initialize mocks before each test by using a setup function
      annotated with @BeforeEach.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,11 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
     testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("io.mockk:mockk:1.13.13")
+
 }
 
 tasks.test {
--- src/test/kotlin/MockitoTest.kt
+++ src/test/kotlin/MockitoTest.kt
@@ -1,75 +1,81 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Test
-import org.mockito.Answers
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.mockitoSession
-import org.mockito.MockitoSession
 import org.mockito.kotlin.whenever
-import org.mockito.kotlin.withSettings
-import org.mockito.quality.Strictness
-import org.mockito.kotlin.mock as mockWithDefaultAnswer
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.mock
+import org.mockito.stubbing.Answer
 
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class NotMockedException : IllegalStateException("not mocked")
+
+class ThrowingDefaultAnswer : Answer<Any> {
+    override fun answer(invocation: org.mockito.invocation.InvocationOnMock?) {
+        throw NotMockedException()
+    }
+}
+
+inline fun <reified T : Any>mockStrictly(): T {
+    return mock<T>(defaultAnswer = ThrowingDefaultAnswer())
+}
+
+@ExtendWith(MockitoExtension::class)
 class MockitoTest {
-    private lateinit var mockito: MockitoSession
-    private lateinit var service: Service
-
-    @BeforeAll
-    fun setup() {
-        service = mock()
-        mockito = mockitoSession().initMocks(this).strictness(Strictness.STRICT_STUBS).startMocking()
-    }
-
-    @AfterAll
-    fun teardown() {
-        mockito.finishMocking()
-    }
-
-    @Test
-    fun `returns default values even if not mocked`() {
-        val serviceStrictStubbing = mock<Service>(withSettings().strictness(Strictness.STRICT_STUBS))
-        val serviceReturnsSmartNulls = mock<Service>(withSettings().defaultAnswer(Answers.RETURNS_SMART_NULLS))
-        val serviceReturnsMocks = mock<Service>(withSettings().defaultAnswer(Answers.RETURNS_MOCKS))
-        val serviceReturnsDefaults = mock<Service>(withSettings().defaultAnswer(Answers.RETURNS_DEFAULTS))
-        val serviceCallsRealMethods = mock<Service>(
-            withSettings().defaultAnswer(
-                Answers.CALLS_REAL_METHODS
-            )
-        )
-        val serviceReturnsDeepStubs = mock<Service>(withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS))
-        val serviceReturnsSelf = mock<Service>(withSettings().defaultAnswer(Answers.RETURNS_SELF))
-
-        assertThat(service(42)).isEqualTo(0)
-        assertThat(serviceStrictStubbing(42)).isEqualTo(0)
-        assertThat(serviceReturnsSmartNulls(42)).isEqualTo(0)
-        assertThat(serviceReturnsMocks(42)).isEqualTo(0)
-        assertThat(serviceReturnsDefaults(42)).isEqualTo(0)
-        assertThat(serviceCallsRealMethods(42)).isEqualTo(0)
-        assertThat(serviceReturnsDeepStubs(42)).isEqualTo(0)
-        assertThat(serviceReturnsSelf(42)).isEqualTo(0)
-    }
 
     private data object NotMockedException : IllegalStateException("not mocked")
 
-    private fun notMocked(): Nothing {
-        throw NotMockedException
+    @Mock
+    private lateinit var serviceMockedWithAnnotation: Service
+
+    private lateinit var serviceMockedStrictly: Service
+
+    @BeforeEach
+    fun setup() {
+        serviceMockedStrictly = mockStrictly<Service>()
     }
 
-    private val serviceWithDefaultAnswer = mockWithDefaultAnswer<Service>(defaultAnswer = { notMocked() })
+
+    // Part 1: Mocking with @Mock annotation is not strict enough
+    @Test
+    fun `@Mock annotated mock fails if parameters do not match`() {
+        whenever(serviceMockedWithAnnotation(43)).thenReturn(12)
+
+        assertThat(serviceMockedWithAnnotation(42)).isEqualTo(0)
+    }
 
     @Test
-    fun `throws exception if not mocked`() {
+    fun `@Mock annotated mock returns default values even if not mocked - This is not as strict as we want it`() {
+        serviceMockedWithAnnotation
+        assertThat(serviceMockedWithAnnotation(42)).isEqualTo(0)
+    }
+
+    // Part 2: Mocking with mockStrictly() is strict
+
+    @Test
+    fun `does fail because mocked stub is not called`() {
+            Mockito.doReturn(42).whenever(serviceMockedStrictly)(42)
+    }
+
+
+    @Test
+    fun `mock with default throws if parameters do not match - test fails because mock was not called`() {
         assertThrows<NotMockedException> {
-            serviceWithDefaultAnswer(42)
+            Mockito.doReturn(42).whenever(serviceMockedStrictly)(42)
+            assertThat(serviceMockedStrictly(43)).isEqualTo(42)
         }
     }
 
     @Test
-    fun `does not throw exception when mocked`() {
-        // this whenever will trigger the default answer leading to an exception before the test even has a chance to run
-        whenever(serviceWithDefaultAnswer(42)).thenReturn(42)
-        assertThat(serviceWithDefaultAnswer(42)).isEqualTo(42)
+    fun `mock with default fails by thrown exception if a method on mock is called and mocking was forgotten`() {
+            serviceMockedStrictly(42)
+    }
+
+    @Test
+    fun `mock with default does not throw exception when a method is called that was explicitly mocked`() {
+        Mockito.doReturn(42).whenever(serviceMockedStrictly)(42)
+        assertThat(serviceMockedStrictly(42)).isEqualTo(42)
     }
 }
